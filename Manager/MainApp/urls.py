7b11b632967cd32bb77c3d13efa9ac63402df6a5
Qusarber <sascha200116@gmail.com>
2021-10-29 23:15:59 +0300

    new changes(log in added)

---
+++ Manager/MainApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('users', UserViewSet)
+
+
+
+urlpatterns = [
+    path('mainapp/', include(router.urls)),
+
+]
