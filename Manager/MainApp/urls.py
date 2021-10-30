d1852a6b974ff9d9c4292e2c1c1ae760247e3b54
Qusarber <sascha200116@gmail.com>
2021-10-30 21:11:23 +0300

    added model Catedories, Smartphone, added method to work with image in debug

--- Manager/MainApp/urls.py
+++ Manager/MainApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from .views import UserViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework.routers import DefaultRouter
+
+from .views import UserViewSet
+
+
 
 
 router = DefaultRouter()
@@ -10,5 +16,7 @@ router.register('users', UserViewSet)
 
 urlpatterns = [
     path('mainapp/', include(router.urls)),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Manager/Manager/settings.py
+++ Manager/Manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -133,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
+
+
+
