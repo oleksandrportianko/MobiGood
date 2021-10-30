c5c37b9ab66f0d666dd511d8a7f67357c669f937
Qusarber <sascha200116@gmail.com>
2021-10-30 18:40:33 +0300

    'auth/' path added (for log in)

--- Manager/Manager/urls.py
+++ Manager/Manager/urls.py
@@ -5,4 +5,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MainApp.urls')),
+    path('auth/', obtain_auth_token),
 ]
