7bf8f3c7ee706476220d0ab5ef751619d7e41683
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2021-11-12 18:45:50 +0200

    again add to git ignore pycache files

--- frontend/src/components/MainContent/Profile/Profile.jsx
+++ frontend/src/components/MainContent/Profile/Profile.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+
+   let email = useSelector((state) => state.auth.email)
+   let firstName = useSelector((state) => state.auth.firstName)
+   let lastName = useSelector((state) => state.auth.lastName)
+
    return (
-      <div>
-         Profile
-      </div>
+      <Container fluid>
+         <Row>
+            <Col>
+               <div>{firstName} {lastName}</div>
+               <div>{email}</div>
+            </Col>
+            <Col>Особисті дані</Col>
+         </Row>
+         <Row>
+            <Col>
+               <ul>
+                  <li>Особисті дані</li>
+                  <li>Мої замовлення</li>
+                  <li>Вибрані товари</li>
+                  <li>Умови повернення і доставки</li>
+                  <li>Про нас</li>
+               </ul>
+            </Col>
+            <Col>Data</Col>
+         </Row>
+      </Container>
    )
 }
 
