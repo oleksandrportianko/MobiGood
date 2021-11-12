acf322c708efdb9bf888b63c58667b2480b8dcce
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2021-11-12 18:15:26 +0200

    add folder pycache to gitignore

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -21,9 +21,19 @@ const Header = (props) => {
       props.logoutUser()
    }
 
+   let setFocusProfileActive = () => {
+      setFocusProfile(true)
+   }
+
+   let setFocusProfileNone = () => {
+      setFocusProfile(false)
+   }
+
    const [showCart, setShowCart] = useState(false);
    const [showLogin, setShowLogin] = useState(false);
    const [showRegistration, setShowRegistration] = useState(false);
+   const [focusProfile, setFocusProfile] = useState(false)
+
    return (
       <Container fluid className="p-0 m-0 ms-md-2">
          <Row className="d-flex p-0 m-0">
@@ -65,10 +75,17 @@ const Header = (props) => {
             />
             {props.isAuth
                ? <Col sm="auto" md={2} lg={2} xl={2} className="header-user-name d-none d-sm-flex p-sm-0 p-md-0 align-items-center justify-content-end">
-                  <Link className="header-profile-none d-flex flex-row align-items-center border border-0" to="/profile">
-                     <p className="m-0 me-2"><Image src={profile} width="28px" height="28px"></Image></p>
+                  <div onMouse={setFocusProfileActive} className="d-flex flex-row align-items-center">
+                     <p className="m-0 me-2"><Image src={profile} width="28px" height="28px" /></p>
                      <p className="header-name-user m-0 me-2">{props.login}</p>
-                  </Link>
+                  </div>
+                  {focusProfile ?
+                     <div className="header-profile-menu-active">
+                        <Link className="header-profile-profile-link d-flex flex-row align-items-center border border-0" to="/profile">
+                           <p>Мій профіль</p>
+                        </Link>
+                     </div>
+                     : null}
                </Col>
                : <Col sm="auto" md={1} lg={1} xl="auto" className="d-none d-sm-flex p-sm-0 p-md-3 align-items-center">
                   <Image width="32px" height="32px" className="header-login-image" src={login} />
