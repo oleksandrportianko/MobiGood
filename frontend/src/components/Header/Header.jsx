7c54558899d1e85727d45f91642d0d7504bae66c
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2021-11-18 14:58:23 +0200

    refactoring header adaptivity

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -23,8 +23,8 @@ const Header = (props) => {
 
    return (
       <Container fluid className="p-0 m-0 ps-md-2">
-         <Row className="d-flex p-1 p-md-0 m-0">
-            <Col sm="auto" md={3} lg={2} xl={2} className="header-logo-container d-none me-md-3 me-lg-5 me-xl-4 p-0 m-0 d-sm-flex align-items-center">
+         <Row className="d-flex flex-row justify-content-around p-1 p-md-0 m-0">
+            <Col xs={1} sm="auto" className="header-logo-container d-none p-0 m-0 d-sm-flex align-items-center">
                <Link to="/">
                   <Image className="header-logo" src={logo} />
                </Link>
@@ -36,7 +36,7 @@ const Header = (props) => {
                </Col>
             </Col>
             <HeaderBurger logoutUser={props.logoutUser} isAuth={props.isAuth} login={props.login} headerItems={props.headerItems} setShowLogin={setShowLogin} setShowRegistration={setShowRegistration} />
-            <Col sm="auto" md={3} lg={4} xl={4} className="header-navigation-container d-none d-sm-flex me-sm-2 me-md-0 me-xl-5 p-0 m-0">
+            <Col xs={4} sm={6} md="auto" lg={4} className="header-navigation-container justify-content-sm-center d-none d-sm-flex p-0 m-0">
                <Nav className="ml-3">
                   <NavLink className="header-nav-link text-decoration-none text-dark" activeClassName="header-nav-active" to="/phones"><Nav.Item className="header-nav-item">Телефони</Nav.Item></NavLink>
                   <NavLink className="text-decoration-none text-dark" activeClassName="header-nav-active" to="/accessories"><Nav.Item className="header-nav-item">Аксесуари</Nav.Item></NavLink>
@@ -44,10 +44,10 @@ const Header = (props) => {
                   <NavLink className="text-decoration-none text-dark" activeClassName="header-nav-active" to="/aboutus"><Nav.Item className="header-nav-item">Про нас</Nav.Item></NavLink>
                </Nav>
             </Col>
-            <Col sm="auto" md={2} lg={2} xl={2} className="header-number-container d-none me-sm-2 me-md-0 p-0 d-sm-flex align-items-center">
+            <Col sm="auto" className="header-number-container d-none p-0 ms-md-2 ms-lg-4 me-lg-4 d-md-flex align-items-center justify-content-center">
                <p className="header-phone-number m-0 fw-bold">(066)-090-36-80</p>
             </Col>
-            <Col xs={6} sm="auto" md={1} lg={1} xl={1} className="header-cart-container p-0 ms-sm-2 me-sm-1 ms-md-0 me-md-0 p-sm-0 p-md-3 me-lg-0 me-xl-2 d-flex justify-content-end d-flex align-items-center">
+            <Col className="header-cart-container p-0 ms-sm-0 ms-lg-2 d-flex justify-content-end justify-content-sm-center d-flex align-items-center">
                <button onClick={() => setShowCart(true)} className="position-relative p-0 border-0 bg-transparent">
                   {props.countItemsCart !== 0 ?
                      <div className="header-cart-circle position-absolute">{props.countItemsCart}</div>
@@ -61,13 +61,13 @@ const Header = (props) => {
                countItemsCart={props.countItemsCart}
             />
             {props.isAuth
-               ? <Col sm="auto" md={2} lg={2} xl={2} className="header-user-name d-none d-sm-flex p-sm-0 p-md-0 ms-sm-4 ms-xl-0 align-items-center justify-content-end">
+               ? <Col className="header-user-name p-0 pe-2 d-none d-sm-flex align-items-center justify-content-end">
                   <Link to="/profile" className="header-profile-profile-link d-flex flex-row align-items-center">
                      <p className="m-0 me-2"><Image className="header-user-image" src={profile} width="28px" height="28px" /></p>
                      <p className="header-name-user m-0 me-0 me-md-2">Профіль</p>
                   </Link>
                </Col>
-               : <Col sm="auto" md={1} lg={1} xl="auto" className="d-none d-sm-flex ms-sm-3 ms-md-0 p-sm-0 p-md-3 align-items-center">
+               : <Col className="d-none d-sm-flex align-items-center justify-content-end">
                   <Image width="32px" height="32px" className="header-login-image" src={login} />
                   <button onClick={() => setShowLogin(true)} className="header-button-login bg-transparent text-black">Вхід</button>
                   <button onClick={() => setShowRegistration(true)} className="header-button-regitration bg-transparent text-black">Реєстрація</button>
