8fe31792fdf8644ae2e192f4bc595c12536d4630
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-03-21 11:39:05 +0200

    append about us page

--- frontend/src/components/MainContent/Phones/Phones.jsx
+++ frontend/src/components/MainContent/Phones/Phones.jsx
@@ -15,11 +15,11 @@ const Phones = () => {
    const phonesData = useSelector((state) => state.phones.phonesData);
    const likedItems = useSelector((state) => state.liked.likedItems);
    const isAuth = useSelector((state) => state.auth.isAuth);
+   const dispatch = useDispatch()
+   const [focus, setFocus] = useState([])
 
    const arrayLikedItems = [];
    const arrayCartItems = [];
-   const dispatch = useDispatch()
-   const [focus, setFocus] = useState([])
 
    if (likedItems) {
       let mapedLikedItems = likedItems.forEach((item) => arrayLikedItems.push(item.id));
@@ -76,14 +76,6 @@ const Phones = () => {
                            : ''
                         }
                         <Image className="me-3" width="180px" height="160px" src={el.image} />
-                     </Col>
-                     <Col className="mt-1 mb-2 d-flex justify-content-center">
-                        {el.color1 && <span className="phones-color-input" style={{ backgroundColor: el.color1 }} />}
-                        {el.color2 && <span className="phones-color-input" style={{ backgroundColor: el.color2 }} />}
-                        {el.color3 && <span className="phones-color-input" style={{ backgroundColor: el.color3 }} />}
-                        {el.color4 && <span className="phones-color-input" style={{ backgroundColor: el.color4 }} />}
-                        {el.color5 && <span className="phones-color-input" style={{ backgroundColor: el.color5 }} />}
-                        {el.color6 && <span className="phones-color-input" style={{ backgroundColor: el.color6 }} />}
                      </Col>
                      <Col className="phones-phone-title mt-1 d-flex justify-content-center text-center">
                         <Link className='phones-phone-title' to={'phone/' + el.id}>
